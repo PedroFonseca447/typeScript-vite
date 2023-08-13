f192ed4d738fe49e0cf9138dc8a3af0a6d0f37de
Pedro B B Fonseca <84049639+PedroFonseca447@users.noreply.github.com>
2023-08-13 02:08:20 -0300

    funcionando

---
+++ src/components/CreateTaskModal/index.tsx
@@ -0,0 +1,43 @@
+import Modal from "react-modal"
+ 
+import { X } from "react-feather"
+ 
+import styles from "./styles.module.css"
+ 
+export function CreateTaskModal({isOpen,onRequestClose}) {
+ 
+  
+
+  return (
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={onRequestClose}
+        overlayClassName="react-modal-overlay"
+        className={styles.container}
+       >
+      <button
+        type="button"
+        onClick={() => onRequestClose()}
+        className={styles.closeButton}
+      >
+        <X size={16} />
+      </button>
+ 
+      <h1>Adicionar tarefa</h1>
+ 
+      <form >
+        <label htmlFor="task">Título da tarefa</label>
+        <input
+          type="text"
+          name="task"
+          placeholder="Digite aqui"
+          value=""
+        />
+ 
+        <button type="submit" className={styles.button}>
+          Adicionar
+        </button>
+      </form>
+    </Modal>
+  )
+}
---
+++ src/components/CreateTaskModal/types.ts
@@ -0,0 +1,2 @@
+//interfaces dos componentes, contratos que eles devem seguir
+//e quais tipos de dados
